2c3513ac1db95e6c17c6d813c86f36fc2ea42eed
DeepakDK04 <deepak99pmk@gmail.com>
2020-12-25 17:02:43 +0530

    organize comments, restructured arrangment of fns

--- youTubeDownloader.py
+++ youTubeDownloader.py
@@ -4,23 +4,50 @@ from easy_table import EasyTable
 URL = ''
 
 
-def getLink():
-    global URL
-    print("\n\n")
-    URL = input("YouTube Vedio URL >>> ")
-    try:
-        vedio = YouTube(URL)
-        availableStream = vedio.streams
-    except Exception as e:
-        print(e)
-        print("Can't get vedio from this Url.")
-    print("\n\n")
-    print("\tLoading...")
-    print("\n\n")
-    getStreams(availableStream)
+def downloadVedio(availableStream, itag):
+    # extract vedio id from URL Query for unique filename.
+    vedio_ID = URL.split('?v=')[1]
+    filename = f"YouTubeVedio-{vedio_ID}"
+    output_path = "../YouTubeVedios/Downloads"
+    print("Downloading...")
+    v = availableStream.get_by_itag(itag)#load the stream selected by user
+    if v:
+        #check and download, if the stream is available
+        v.download(output_path=output_path,
+                   filename=filename)
+    else:
+        print("Error, Can't Initiate Download Process")
+    print("Download Complete")
+
+
+def streamTableDisplay(table_data):
+    table = EasyTable("Available Stream Types")
+    table.setCorners("/", "\\", "\\", "/")
+    table.setOuterStructure("|", "-")
+    table.setInnerStructure("|", "-", "+")
+    table.setData(table_data)  # setting a table object with given fields.
+    print()
+    table.displayTable()  # display the table for user.
+    print()
+
+
+def chooseStream(availableStream_Itags):
+    # user choice for which stream to be downloaded.
+    print("Choose Any ID from above Table to Download it\n")
+    print("Enter ID to Download (or) Enter 0 to Cancel")
+    user_choice = str(input("Enter your Choice >>> "))
+    # errror cases
+    if user_choice == 0:
+        return "cancel"
+    if user_choice not in availableStream_Itags:
+        print("Invalid Choice, Please enter a Vaid One\n\n")
+        chooseStream(availableStream_Itags)
+    # success cases
+    return user_choice
 
 
 def getStreamTag(streamObject, attribute):
+    # get the value for the key in stream object, if can't get mark as un available.
     try:
         if attribute == 'itag':
             value = streamObject.itag
@@ -37,12 +64,13 @@ def getStreamTag(streamObject, attribute):
         else:
             value = ''
     except Exception as e:
-        print(e)
+        # print(e)
         value = "Not Available"
     return str(value)
 
 
 def getStream(stream):
+    # serialize the stream object and took only required fields.
     stream_info = {}
     stream_info["    ID    "] = getStreamTag(stream, "itag")
     stream_info["   Type   "] = getStreamTag(stream, "type")
@@ -61,6 +89,7 @@ def getStreams(availableStream):
         # availableStream_Itags used later to select the file to download
         availableStream_Itags.append(stream_info.get("    ID    ", None))
         StreamList.append(stream_info)
+        # construct available streams in a list of dictionaries to generate table display.
     streamTableDisplay(StreamList)
     choice = chooseStream(availableStream_Itags)
     if choice == "cancel":
@@ -70,42 +99,24 @@ def getStreams(availableStream):
         downloadVedio(availableStream, choice)
 
 
-def streamTableDisplay(table_data):
-    table = EasyTable("Available Stream Types")
-    table.setCorners("/", "\\", "\\", "/")
-    table.setOuterStructure("|", "-")
-    table.setInnerStructure("|", "-", "+")
-    table.setData(table_data)
-    print()
-    table.displayTable()
-    print()
+def getLink():
+    global URL
+    print("\n\n")
+    URL = input("YouTube Vedio URL >>> ")
+    try:
+        # extract stream object from the vedio object to download in file stream
+        vedio = YouTube(URL)
+        availableStream = vedio.streams
+    except Exception as e:
+        print("Error!")
+        print(e)
+        print("Can't get vedio from this Url.")
+    print("\n\n")
+    print("\tLoading...")
+    print("\n\n")
+    getStreams(availableStream)
 
 
-def chooseStream(availableStream_Itags):
-    print("Choose Any ID from above Table to Download it\n")
-    print("Enter ID to Download (or) Enter 0 to Cancel")
-    user_choice = str(input("Enter your Choice >>> "))
-    # errror cases
-    if user_choice == 0:
-        return "cancel"
-    if user_choice not in availableStream_Itags:
-        print("Invalid Choice, Please enter a Vaid One\n\n")
-        chooseStream(availableStream_Itags)
-    # success cases
-    return user_choice
-
-
-def downloadVedio(availableStream, itag):
-    vedio_ID = URL.split('?v=')[1]  # extract vedio id from URL Query
-    print("Downloading...")
-    v = availableStream.get_by_itag(itag)
-    if v:
-        v.download(output_path="../YouTubeVedios/Downloads",
-                   filename=f"YouTubeVedio-{vedio_ID}")
-    else:
-        print("Error, Can't Initiate Download Process")
-    print("Download Complete")
-
-
-getLink()  # only works with YOUTUBE Vedio links Not suitable for PLAYLIST links
-test_vedio_link = "https://www.youtube.com/watch?v=l73dA-A0Si4"  # tested OK
+# only works with YOUTUBE Vedio links Not suitable for PLAYLIST links
+if __name__ == "__main__":
+    getLink()
